6dd7b73739a2f0e7a2da3edc26ddfa0f7a08e4ec
Joseph Tummon <96840768+JosephTummon@users.noreply.github.com>
2022-11-01 15:36:07 +0000

    Add files via upload

---
+++ p2p4.py
@@ -0,0 +1,25 @@
+animal = "elephant"
+
+letter = animal[0]
+print(letter)
+
+letter = animal[1]
+print(letter)
+
+letter = animal[2]
+print(letter)
+
+letter = animal[3]
+print(letter)
+
+letter = animal[4]
+print(letter)
+
+letter = animal[5]
+print(letter)
+
+letter = animal[6]
+print(letter)
+
+letter = animal[7]
+print(letter)
