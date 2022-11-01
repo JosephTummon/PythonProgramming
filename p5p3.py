008ff7ddfb23bdaf6e46107ba1ab82e8603159e7
Joseph Tummon <96840768+JosephTummon@users.noreply.github.com>
2022-11-01 15:44:08 +0000

    Delete p5p3.py

--- p5p3.py
+++
@@ -1,8 +0,0 @@
-number = float(input("Please enter any number: "))
-
-if number == 0:
-    print("This number is equal to 0")
-elif number < 0:
-    print("This is a negative number")
-else:
-    print("This is a positive number")
